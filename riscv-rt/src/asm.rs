970d21a39a38870609d620c039ebcad258c51792
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-04-29 14:27:24 +0300

    HACK(RVE): use a5 instead of t3 for single-hart jump
    RVE does not support registers x16--x31, including t3 (x28). I replaced
    the use of t3 in the single-hart jump with a5 (x15) to make it compile.

--- riscv-rt/src/asm.rs
+++ riscv-rt/src/asm.rs
@@ -121,9 +121,9 @@ cfg_global_asm!(
     #[cfg(not(riscvm))]
     "beqz t2, 2f  // Jump if single-hart
     mv t1, t2
-    mv t3, t0
+    mv a5, t0
 1:
-    add t0, t0, t3
+    add t0, t0, a5
     addi t1, t1, -1
     bnez t1, 1b
 2:  ",
